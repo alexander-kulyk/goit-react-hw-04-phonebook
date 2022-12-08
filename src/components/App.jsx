2ac31ff5031eaf7c630674292c64d19961fe3020
alexander-kulyk <kulyk.alexander@gmail.com>
2022-12-08 20:09:57 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { nanoid } from 'nanoid';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
